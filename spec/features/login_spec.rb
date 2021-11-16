d9a362376dc2bd9ebb94d995c1d38c291856a1f8
MattW <mattnwarnock@yahoo.co.uk>
2021-11-16 12:43:50 +0000

    refactor using helper

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
+require 'web_helpers'
 
 RSpec.describe 'User', type: :system do
   describe 'Login' do
     it 'allows user to log in' do
-      visit root_path
-      click_link 'Login'
-      fill_in(:exampleInputEmail1, with: 'm@p.com')
-      fill_in(:exampleInputPassword1, with: '12345678')
-      click_button
+      login
       expect(page).to have_content 'Welcome Mary!'
     end
   end
 end
-
