3d67ebd5a5f95bc8d74c18d153ac0223433ab87f
MattW <mattnwarnock@yahoo.co.uk>
2021-11-16 15:50:52 +0000

    refactored and added tests for no context

--- spec/features/new_post_spec.rb
+++ spec/features/new_post_spec.rb
@@ -5,13 +5,32 @@ RSpec.describe 'Post', type: :system do
   describe 'New Post' do
     it 'allows user to create a new post' do
       login
-      visit root_path
-      click_link 'New Post'
-      fill_in(:post_content, with: 'This is a test post!')
-      attach_file('post_image', Rails.root + 'spec/test_image.png')
-      click_button
+      create_post_with_img
+
       expect(page).to have_content 'Post was successfully created.'
     end
+
+    context 'when picture is added to post' do
+      it 'allows no text to be added' do
+        login
+        click_link 'New Post'
+        attach_file('post_image', Rails.root + 'spec/test_image.png')
+        click_button
+
+        expect(page).to_not have_content 'cannot be empty'
+        expect(page).to have_content 'Post was successfully created.'
+      end
+    end
+
+    context 'when not picture is added to post' do
+      xit 'displays error message if no text added' do
+        login
+        click_link 'New Post'
+        click_button
+
+        expect(page).to have_content 'cannot be empty'
+      end
+    end
+
   end
 end
-
