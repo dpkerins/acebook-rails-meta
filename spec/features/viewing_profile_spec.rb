2b01b998d48a87b33f97cb5334a8f77fe03a54ec
Douglas <douglaskerins@gmail.com>
2021-11-17 10:57:56 +0000

    adding viewing profile feature test

---
+++ spec/features/viewing_profile_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require 'web_helpers'
+
+RSpec.describe 'User', type: :system do
+  describe 'viewing profile page' do
+    before(:each) do
+      login
+      create_post_with_img
+      visit root_path
+      create_comment
+    end
+    it 'allows user to view their profile page' do
+      find("#dropdownUser1").click
+      click_link "Profile"
+      expect(page).to have_content 'Welcome Mary!'
+      expect(page).to have_content 'm@p.com'
+      expect(page).to have_content 'Recent posts'
+      expect(page).to have_content 'This is a test post!'
+      expect(page).to have_content 'This is a test comment'
+    end
+  end
+end
+
