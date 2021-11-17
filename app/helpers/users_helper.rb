5904455b3f23e7ebb3b9a522ae5b2dbf3cae0aad
Alex Boyling <alexboyling@gmail.com>
2021-11-17 17:06:55 +0000

    add link to original post

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   def find_post_for_comment(id)
-    post = Post.find(id)
+    Post.find(id)
   end
 
   def avatar_for(id)
@@ -10,6 +10,15 @@ module UsersHelper
       image_tag(user, class:"rounded-circle", alt:"avatar", style:"object-fit:cover; height:50px; width:50px;")
     else
       image_tag("avatar_placeholder.png", class:"rounded-circle", alt:"avatar", style:"object-fit:cover; height:50px; width:50px;")
+    end
+  end
+
+  def big_avatar_for(id)
+    user = User.find(id).avatar
+    if user.attached?
+      image_tag(user, class:"rounded-circle", alt:"avatar", style:"object-fit:cover; height:200px; width:200px;")
+    else
+      image_tag("avatar_placeholder.png", class:"rounded-circle", alt:"avatar", style:"object-fit:cover; height:200px; width:200px;")
     end
   end
 
